f8f8caf27778a2c2719dbaf65f335a71441e11dd
Protocol-i <protocoliddriss@gmail.com>
2023-10-05 02:53:46 -0700

    basic shell commands 0.1

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 100
+void prj_run_shell(void);
+void prj_execute_command(const char *command);
+void prj_display_prompt(void);
+
+
+#endif
+
